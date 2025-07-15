9bda411ecd520cf7885f9f66327803906f66adfb
Alex Lamper <143872753+AlexLamper@users.noreply.github.com>
2025-07-15 21:35:36 +0200

    fix: align project page props with Next.js type

--- app/project/[slug]/page.tsx
+++ app/project/[slug]/page.tsx
@@ -1,5 +1,6 @@
 import ProjectOverlay from '../../components/ProjectOverlay'
+import type { PageProps } from 'next'
 
-export default function ProjectPage({ params }: { params: { slug: string } }) {
+export default function ProjectPage({ params }: PageProps<{ slug: string }>) {
   return <ProjectOverlay slug={params.slug} />
 }
